c39cf78a1102678edacbe395c0409fb7c4aa2b55
vikrant1717 <vikrantphadtare9@gmail.com>
2024-09-09 15:14:27 +0530

    first

---
+++ src/maths utils/add.java
@@ -0,0 +1,18 @@
+public class add {
+
+    public void sum()
+    {
+        int a=10;
+        int b=20;
+
+        int c = a + b;
+
+        System.out.println("value of c is :" + c);
+
+    }
+
+    public static void main(String[] args) {
+        add obj = new add();
+        obj.sum();
+    }
+}
