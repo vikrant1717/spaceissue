8bf74dbe0ef1eddb65fd982845102d8c229d3e29
vikrant11 <vikrantphadtare9@gmail.com>
2024-09-13 18:03:31 +0530

    Update add.java

--- src/maths utils /add.java
+++ src/maths utils /add.java
@@ -7,7 +7,7 @@ public class add {
 
         int c = a + b;
 
-       // System.out.println("value of c is :" + c);
+        System.out.println("value of c is :" + c); //added in second snaps
 
     }
 
@@ -18,7 +18,7 @@ public class add {
 
         int c = a - b;
 
-        System.out.println("value of c is :" + c);
+       // System.out.println("value of c is :" + c); //commented in second snapshot 
 
     }
 
