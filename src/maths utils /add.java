14928ce86469b9e701e672b15f65f31ea1217f5a
vikrant11 <vikrantphadtare9@gmail.com>
2024-09-13 17:52:54 +0530

    Update add.java

--- src/maths utils /add.java
+++ src/maths utils /add.java
@@ -11,8 +11,20 @@ public class add {
 
     }
 
+    public void sub()
+    {
+        int a=100;
+        int b=20;
+
+        int c = a - b;
+
+        System.out.println("value of c is :" + c);
+
+    }
+
     public static void main(String[] args) {
         add obj = new add();
         obj.sum();
+        obj.sub();
     }
 }
